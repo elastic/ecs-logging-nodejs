992e42b3ed4c44afcfca3f4cfe0ce52f7b9e5b40
delvedor <tommydelved@gmail.com>
2020-01-28 14:33:04 +0100

    Updated test

--- loggers/winston/test.js
+++ loggers/winston/test.js
@@ -98,3 +98,80 @@ test.cb('http request and response (req, res keys)', t => {
     res.end('ok')
   }
 })
+
+test.cb('http request and response (request, response keys)', t => {
+  t.plan(2)
+
+  class TestTransport extends Transport {
+    log (info, callback) {
+      const line = JSON.parse(info[MESSAGE])
+      t.true(validate(line))
+      callback()
+    }
+  }
+
+  const logger = winston.createLogger({
+    level: 'info',
+    format: ecsFormat(),
+    transports: [new TestTransport()]
+  })
+
+  const server = stoppable(http.createServer(handler))
+  server.listen(0, () => {
+    const body = JSON.stringify({ hello: 'world' })
+    sget({
+      method: 'POST',
+      url: `http://localhost:${server.address().port}?foo=bar`,
+      body,
+      headers: {
+        'user-agent': 'cool-agent',
+        'content-type': 'application/json',
+        'content-length': Buffer.byteLength(body)
+      }
+    }, (err, res) => {
+      t.falsy(err)
+      server.stop()
+      t.end()
+    })
+  })
+
+  function handler (request, response) {
+    // test also the anchor
+    request.url += '#anchor'
+    logger.info('incoming request', { request, response })
+    response.end('ok')
+  }
+})
+
+// waiting on https://github.com/fastify/fast-json-stringify/pull/206
+test.skip('Keys order', t => {
+  t.plan(2)
+
+  var count = 0
+  class TestTransport extends Transport {
+    log (info, callback) {
+      const line = JSON.parse(info[MESSAGE])
+      if (count++ === 0) {
+        t.is(
+          info[MESSAGE],
+          `{"@timestamp":"${line['@timestamp']}","log":{"level":"info","logger":"winston"},"message":"ecs is cool!","ecs":{"version":"1.4.0"}}`
+        )
+      } else {
+        t.is(
+          info[MESSAGE],
+          `{"@timestamp":"${line['@timestamp']}","log":{"level":"error","logger":"winston"},"message":"ecs is cool!","ecs":{"version":"1.4.0"},"hello":"world"}`
+        )
+      }
+      callback()
+    }
+  }
+
+  const logger = winston.createLogger({
+    level: 'info',
+    format: ecsFormat(),
+    transports: [new TestTransport()]
+  })
+
+  logger.info('ecs is cool!')
+  logger.error('ecs is cool!', { hello: 'world' })
+})
