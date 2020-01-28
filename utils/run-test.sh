c824388daa28b3753163ba79782693b26ea0fe3d
delvedor <tommydelved@gmail.com>
2020-01-28 16:28:24 +0100

    Added a single command to run all test

---
+++ utils/run-test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+ROOT=$PWD
+
+function run_test {
+  echo "=== Running $1 test ==="
+  cd $1
+  npm install
+  npm test
+  cd $ROOT
+}
+
+run_test "helper"
+run_test "loggers/winston"
+run_test "loggers/morgan"
