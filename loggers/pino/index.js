638ee26f435dc4bac74f526986dc86d2c7797e22
delvedor <tommydelved@gmail.com>
2020-03-16 10:19:32 +0100

    Export a builder function

--- loggers/pino/index.js
+++ loggers/pino/index.js
@@ -9,52 +9,54 @@ const {
   formatHttpResponse
 } = require('@elastic/ecs-helpers')
 
-const pinoOptions = {
-  formatters: {
-    level (label, number) {
-      return {
-        log: {
-          level: label,
-          logger: 'pino'
+function build () {
+  return {
+    formatters: {
+      level (label, number) {
+        return {
+          log: {
+            level: label,
+            logger: 'pino'
+          }
         }
+      },
+
+      bindings (bindings) {
+        return {
+          process: {
+            pid: bindings.pid
+          },
+          host: {
+            hostname: bindings.hostname
+          }
+        }
+      },
+
+      log (obj) {
+        const {
+          req,
+          request,
+          res,
+          response,
+          ...ecs
+        } = obj
+
+        ecs.ecs = { version: '1.5.0' }
+
+        if (req || request) {
+          formatHttpRequest(ecs, req || request)
+        }
+
+        if (res || response) {
+          formatHttpResponse(ecs, res || response)
+        }
+
+        return ecs
       }
     },
-
-    bindings (bindings) {
-      return {
-        process: {
-          pid: bindings.pid
-        },
-        host: {
-          hostname: bindings.hostname
-        }
-      }
-    },
-
-    log (obj) {
-      const {
-        req,
-        request,
-        res,
-        response,
-        ...ecs
-      } = obj
-
-      ecs.ecs = { version: '1.5.0' }
-
-      if (req || request) {
-        formatHttpRequest(ecs, req || request)
-      }
-
-      if (res || response) {
-        formatHttpResponse(ecs, res || response)
-      }
-
-      return ecs
-    }
-  },
-  messageKey: 'message',
-  timestamp: () => `,"@timestamp":"${new Date().toISOString()}"`
+    messageKey: 'message',
+    timestamp: () => `,"@timestamp":"${new Date().toISOString()}"`
+  }
 }
 
-module.exports = pinoOptions
+module.exports = build
