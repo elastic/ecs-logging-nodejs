d40bfad2255380c4517f47f1da6aa20a85d93737
Trent Mick <trent.mick@elastic.co>
2021-01-18 11:30:20 -0800

    test(morgan) change from ava to tap for testing
    See #29 for a discussion of why.

--- loggers/morgan/test.js
+++ loggers/morgan/test.js
@@ -6,14 +6,11 @@
 
 const http = require('http')
 
-const test = require('ava')
-const sget = require('simple-get')
 const Ajv = require('ajv')
 const express = require('express')
 const morgan = require('morgan')
-const stoppable = require('stoppable')
 const split = require('split2')
-const { version } = require('@elastic/ecs-helpers')
+const test = require('tap').test
 
 const ecsFormat = require('./')
 
@@ -24,178 +21,124 @@ const ajv = Ajv({
 })
 const validate = ajv.compile(require('../../utils/schema.json'))
 
-test.cb('Should produce valid ecs logs', t => {
+// 1. Make an Express server using a given morgan ECS format `logger`.
+// 2. Make a canned `POST /?foo=bar` request against.
+// 3. Shutdown the server and callback `cb(err)`.
+function makeExpressServerAndRequest (logger, cb) {
+  const app = express()
+  app.use(logger)
+  app.use('/', (req, res) => {
+    res.end('ok')
+  })
+
+  const server = app.listen(0, () => {
+    const body = JSON.stringify({ hello: 'world' })
+    const req = http.request(
+      `http://localhost:${server.address().port}?foo=bar`,
+      {
+        method: 'POST',
+        headers: {
+          'user-agent': 'cool-agent',
+          'content-type': 'application/json',
+          'content-length': Buffer.byteLength(body)
+        }
+      },
+      function (res) {
+        res.on('data', function () {})
+        res.on('close', function () {
+          server.close(cb)
+        })
+      }
+    )
+    req.write(body)
+    req.end()
+  })
+}
+
+test('Should produce valid ecs logs', t => {
   t.plan(2)
 
   const stream = split(JSON.parse).on('data', line => {
     t.true(validate(line))
   })
+  const logger = morgan(ecsFormat(), { stream })
 
-  const app = express()
-  app.use(morgan(ecsFormat(), { stream }))
-  app.use('/', (req, res) => {
-    res.end('ok')
+  makeExpressServerAndRequest(logger, function (err) {
+    t.ifErr(err)
+    t.end()
   })
-
-  const server = stoppable(app.listen(0, () => {
-    const body = JSON.stringify({ hello: 'world' })
-    sget({
-      method: 'POST',
-      url: `http://localhost:${server.address().port}?foo=bar`,
-      body,
-      headers: {
-        'user-agent': 'cool-agent',
-        'content-type': 'application/json',
-        'content-length': Buffer.byteLength(body)
-      }
-    }, (err, res) => {
-      t.falsy(err)
-      server.stop()
-      t.end()
-    })
-  }))
 })
 
-test.cb('Keys order', t => {
+test('Keys order', t => {
   t.plan(2)
 
   const stream = split().on('data', line => {
-    const log = JSON.parse(line)
-    t.is(line, `{"@timestamp":"${log['@timestamp']}","log.level":"info","message":"${JSON.stringify(log.message).slice(1, -1)}","ecs":{"version":"${version}"},"http":{"version":"1.1","request":{"method":"post","headers":{"accept-encoding":"gzip, deflate","content-type":"application/json","host":"${log.http.request.headers.host}","connection":"close"},"body":{"bytes":17}},"response":{"status_code":200,"headers":{"x-powered-by":"Express"}}},"url":{"path":"/","domain":"localhost","query":"foo=bar","full":"http://localhost:${server.address().port}/?foo=bar"},"user_agent":{"original":"cool-agent"}}`)
+    const rec = JSON.parse(line)
+    const expectedLineStart = `{"@timestamp":"${rec['@timestamp']}","log.level":"info","message":"${JSON.stringify(rec.message).slice(1, -1)}",`
+    t.equal(line.slice(0, expectedLineStart.length), expectedLineStart,
+      'log line starts with the expected @timestamp, log.level, and message fields')
   })
+  const logger = morgan(ecsFormat(), { stream })
 
-  const app = express()
-  app.use(morgan(ecsFormat(), { stream }))
-  app.use('/', (req, res) => {
-    res.end('ok')
+  makeExpressServerAndRequest(logger, function (err) {
+    t.ifErr(err)
+    t.end()
   })
-
-  const server = stoppable(app.listen(0, () => {
-    const body = JSON.stringify({ hello: 'world' })
-    sget({
-      method: 'POST',
-      url: `http://localhost:${server.address().port}?foo=bar`,
-      body,
-      headers: {
-        'user-agent': 'cool-agent',
-        'content-type': 'application/json',
-        'content-length': Buffer.byteLength(body)
-      }
-    }, (err, res) => {
-      t.falsy(err)
-      server.stop()
-      t.end()
-    })
-  }))
 })
 
-test.cb('"format" argument - format name', t => {
-  t.plan(1)
+test('"format" argument - format name', t => {
+  t.plan(2)
 
   // Example:
-  //  GET /?foo=bar 200 - - 0.073 ms
+  //  POST /?foo=bar 200 - - 0.073 ms
   const format = 'tiny' // https://github.com/expressjs/morgan#tiny
-  const msgRe = /^GET \/\?foo=bar 200 - - \d+\.\d+ ms$/
+  const msgRe = /^POST \/\?foo=bar 200 - - \d+\.\d+ ms$/
   const stream = split().on('data', line => {
     const rec = JSON.parse(line)
-    t.true(msgRe.test(rec.message),
-      `rec.message ${JSON.stringify(rec.message)} matches ${msgRe}`)
+    t.match(rec.message, msgRe, 'rec.message')
   })
+  const logger = morgan(ecsFormat(format), { stream })
 
-  // Express app using the given morgan `format`.
-  const app = express()
-  app.use(morgan(ecsFormat(format), { stream }))
-  app.use('/', (req, res) => {
-    res.end('ok')
-  })
-
-  // Make a request, then end the test.
-  const server = app.listen(0, () => {
-    const req = http.request(
-      `http://localhost:${server.address().port}?foo=bar`,
-      function (res) {
-        res.on('data', function () {})
-        res.on('close', function () {
-          server.close(function () {
-            t.end()
-          })
-        })
-      }
-    )
-    req.end()
+  makeExpressServerAndRequest(logger, function (err) {
+    t.ifErr(err)
+    t.end()
   })
 })
 
-test.cb('"format" argument - format string', t => {
-  t.plan(1)
+test('"format" argument - format string', t => {
+  t.plan(2)
 
   // Example:
-  //  GET /?foo=bar 200
+  //  POST /?foo=bar 200
   const format = ':method :url :status'
-  const msgRe = /^GET \/\?foo=bar 200$/
+  const msgRe = /^POST \/\?foo=bar 200$/
   const stream = split().on('data', line => {
     const rec = JSON.parse(line)
-    t.true(msgRe.test(rec.message),
-      `rec.message ${JSON.stringify(rec.message)} matches ${msgRe}`)
+    t.match(rec.message, msgRe, 'rec.message')
   })
+  const logger = morgan(ecsFormat(format), { stream })
 
-  // Express app using the given morgan `format`.
-  const app = express()
-  app.use(morgan(ecsFormat(format), { stream }))
-  app.use('/', (req, res) => {
-    res.end('ok')
-  })
-
-  // Make a request, then end the test.
-  const server = app.listen(0, () => {
-    const req = http.request(
-      `http://localhost:${server.address().port}?foo=bar`,
-      function (res) {
-        res.on('data', function () {})
-        res.on('close', function () {
-          server.close(function () {
-            t.end()
-          })
-        })
-      }
-    )
-    req.end()
+  makeExpressServerAndRequest(logger, function (err) {
+    t.ifErr(err)
+    t.end()
   })
 })
 
-test.cb('"format" argument - format function', t => {
-  t.plan(1)
+test('"format" argument - format function', t => {
+  t.plan(2)
 
   // Example:
-  //  GET /?foo=bar 200 - - 0.073 ms
+  //  POST /?foo=bar 200 - - 0.073 ms
   const format = morgan.tiny
-  const msgRe = /^GET \/\?foo=bar 200 - - \d+\.\d+ ms$/
+  const msgRe = /^POST \/\?foo=bar 200 - - \d+\.\d+ ms$/
   const stream = split().on('data', line => {
     const rec = JSON.parse(line)
-    t.true(msgRe.test(rec.message),
-      `rec.message ${JSON.stringify(rec.message)} matches ${msgRe}`)
+    t.match(rec.message, msgRe, 'rec.message')
   })
+  const logger = morgan(ecsFormat(format), { stream })
 
-  // Express app using the given morgan `format`.
-  const app = express()
-  app.use(morgan(ecsFormat(format), { stream }))
-  app.use('/', (req, res) => {
-    res.end('ok')
-  })
-
-  // Make a request, then end the test.
-  const server = app.listen(0, () => {
-    const req = http.request(
-      `http://localhost:${server.address().port}?foo=bar`,
-      function (res) {
-        res.on('data', function () {})
-        res.on('close', function () {
-          server.close(function () {
-            t.end()
-          })
-        })
-      }
-    )
-    req.end()
+  makeExpressServerAndRequest(logger, function (err) {
+    t.ifErr(err)
+    t.end()
   })
 })
