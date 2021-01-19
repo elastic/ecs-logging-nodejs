e4c090cc46347f8d509eb0bbdfbfd8e4668f0fe0
Trent Mick <trent.mick@elastic.co>
2021-01-19 14:53:40 -0800

    feat(pino): add ECS tracing fields if APM is in use (#36)
    Refs: #35

--- loggers/pino/CHANGELOG.md
+++ loggers/pino/CHANGELOG.md
@@ -2,6 +2,11 @@
 
 ## Unreleased
 
+- Add support for [ECS tracing fields](https://www.elastic.co/guide/en/ecs/current/ecs-tracing.html).
+  If it is detected that [Elastic APM](https://www.npmjs.com/package/elastic-apm-node)
+  is in use and there is an active trace, then tracing fields will be added to
+  log records. This enables linking between traces and log records in Kibana.
+
 - BREAKING CHANGE: Conversion of HTTP request and response objects is no longer
   done by default. One must use the new `convertReqRes: true` formatter option.
   As well, only the meta keys `req` and `res` will be handled. Before this
---
+++ loggers/pino/test/serve-one-http-req-with-apm.js
@@ -0,0 +1,50 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+'use strict'
+
+// This script is used to test @elastic/ecs-pino-format + APM.
+//
+// It will:
+// - configure APM using the given APM server url (first arg)
+// - start an HTTP server
+// - log once when it is listening (with its address)
+// - handle a single HTTP request
+// - log that request
+// - flush APM (i.e. ensure it has sent its data to its configured APM server)
+// - exit
+
+const serverUrl = process.argv[2]
+/* eslint-disable-next-line no-unused-vars */
+const apm = require('elastic-apm-node').start({
+  serverUrl,
+  serviceName: 'ecs-pino-format-test-apm',
+  centralConfig: false,
+  captureExceptions: false,
+  metricsInterval: 0
+})
+
+const http = require('http')
+const ecsFormat = require('../') // @elastic/ecs-pino-format
+const pino = require('pino')
+
+const log = pino({ ...ecsFormat({ convertReqRes: true }) })
+
+const server = http.createServer()
+
+server.once('request', function handler (req, res) {
+  var span = apm.startSpan('auth')
+  setImmediate(function doneAuth () {
+    span.end()
+    res.end('ok')
+    log.info({ req, res }, 'handled request')
+    apm.flush(function onFlushed () {
+      server.close()
+    })
+  })
+})
+
+server.listen(0, () => {
+  log.info({ address: `http://localhost:${server.address().port}` }, 'listening')
+})
